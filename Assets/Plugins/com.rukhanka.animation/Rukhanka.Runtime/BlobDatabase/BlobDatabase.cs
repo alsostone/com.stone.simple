496e61fde659ae29134ccfe4b6ad46b2436a9130
alsostone <alsostone.163.com>
2025-01-07 18:51:06 +0800

    ﻿Add RukhankaAnimation

---
+++ Assets/Plugins/com.rukhanka.animation/Rukhanka.Runtime/BlobDatabase/BlobDatabase.cs
@@ -0,0 +1,41 @@
+using Unity.Collections;
+using Unity.Entities;
+using Hash128 = Unity.Entities.Hash128;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+[assembly: RegisterGenericComponentType(typeof(Rukhanka.NewBlobAssetDatabaseRecord<Rukhanka.AnimationClipBlob>))]
+[assembly: RegisterGenericComponentType(typeof(Rukhanka.NewBlobAssetDatabaseRecord<Rukhanka.AvatarMaskBlob>))]
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Rukhanka
+{
+
+public struct BlobDatabaseSingleton: IComponentData
+{
+    public NativeHashMap<Hash128, BlobAssetReference<AnimationClipBlob>> animations;
+    public NativeHashMap<Hash128, BlobAssetReference<AvatarMaskBlob>> avatarMasks;
+    
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static unsafe bool IsBlobValid<T>(BlobAssetReference<T> blob) where T: unmanaged
+    {
+        if (!blob.IsCreated)
+            return true;
+        
+        var rv = blob.m_data.Header->ValidationPtr == blob.m_data.m_Ptr;
+        return rv;
+    }
+}
+
+//=================================================================================================================//
+
+public struct NewBlobAssetDatabaseRecord<T>: IBufferElementData where T: unmanaged
+{
+    public Hash128 hash;
+    public BlobAssetReference<T> value;
+}
+
+}
+
